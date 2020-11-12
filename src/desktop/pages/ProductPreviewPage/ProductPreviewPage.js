3c426da0deb4031b96e93a51d9cc948c39a7e33b
Nikolas9898 <nikolanikolchov@gmail.com>
2020-11-12 16:53:02 +0200

    added functionallity in front modules

--- src/desktop/pages/ProductPreviewPage/ProductPreviewPage.js
+++ src/desktop/pages/ProductPreviewPage/ProductPreviewPage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "../../layout/Header/Header";
 import ProductPreviewContainer from "../../modules/product/productPreview/ProductPreviewContainer";
-import ImageGallery from 'react-image-gallery';
+import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/css/image-gallery.css";
 function ProductPreviewPage(props) {
   return (
     <React.Fragment>
-       <Header/>
-        <ProductPreviewContainer/>
+      <Header />
+
+      <ProductPreviewContainer slug={props.match.params.slug} />
     </React.Fragment>
   );
 }
