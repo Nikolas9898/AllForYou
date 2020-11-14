2e5fa61592ad4c1510be8afe8fb3ab52c8444cea
Nikolas9898 <nikolanikolchov@gmail.com>
2020-11-14 22:46:40 +0200

    seo

--- src/desktop/modules/product/productPreview/ProductPreviewContainer.js
+++ src/desktop/modules/product/productPreview/ProductPreviewContainer.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import axios from "axios";
 import ProductPreview from "./components/ProductPreview/ProductPreview";
+import { Helmet } from "react-helmet";
 
 function ProductPreviewContainer({ slug }) {
   const [product, setProduct] = useState({});
@@ -34,6 +35,20 @@ function ProductPreviewContainer({ slug }) {
 
   return (
     <React.Fragment>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{props.match.params.slug}</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta
+          name="og:image"
+          content="http://reedbarger.com/static/ca-styled-logo-5205dbb0983e1531d228c98584ab5711.png"
+        />
+        <meta
+          name="og:ulr"
+          content={`https://zavsekiponeshto.herokuapp.com/product/${props.match.params.slug}`}
+        />
+        <meta name="og:type" content="article" />
+      </Helmet>
       <ProductPreview product={product} previewImages={previewImages} />
     </React.Fragment>
   );
