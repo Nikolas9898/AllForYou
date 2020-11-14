b47ca769daf2273b67e57662f69e7be30fe2d5e5
Nikolas9898 <nikolanikolchov@gmail.com>
2020-11-14 23:25:54 +0200

    seo

--- src/desktop/pages/ProductPreviewPage/ProductPreviewPage.js
+++ src/desktop/pages/ProductPreviewPage/ProductPreviewPage.js
@@ -7,24 +7,9 @@ import { Helmet } from "react-helmet";
 function ProductPreviewPage(props) {
   return (
     <React.Fragment>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>{props.match.params.slug}</title>
-        <link rel="canonical" href="http://mysite.com/example" />
-        <meta
-          name="og:image"
-          content="http://reedbarger.com/static/ca-styled-logo-5205dbb0983e1531d228c98584ab5711.png"
-        />
-        <meta
-          name="og:ulr"
-          content={`https://zavsekiponeshto.herokuapp.com/product/${props.match.params.slug}`}
-        />
-        <meta name="og:type" content="article" />
-
-        {/* <Header />
-        <ProductPreviewContainer slug={props.match.params.slug} />
-        <Footer /> */}
-      </Helmet>
+      <Header />
+      <ProductPreviewContainer slug={props.match.params.slug} />
+      <Footer />
     </React.Fragment>
   );
 }
