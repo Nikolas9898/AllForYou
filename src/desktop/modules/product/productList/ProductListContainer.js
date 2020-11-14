8bdd352cc1a6cc9f42e79ce7539db0e472977d0f
Nikolas9898 <nikolanikolchov@gmail.com>
2020-11-14 21:35:19 +0200

    deleted meta tag

--- src/desktop/modules/product/productList/ProductListContainer.js
+++ src/desktop/modules/product/productList/ProductListContainer.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import ProductListContainerStyle from "./ProductListContainerStyle.module.css";
 import axios from "axios";
-import ContentLoader from 'react-content-loader'
+import ContentLoader from "react-content-loader";
 
 function ProductListContainer(props) {
   const [products, setProducts] = useState([]);
-  const [loader] = useState([{},{},{},{}]);
+  const [loader] = useState([{}, {}, {}, {}]);
   useEffect(() => {
     axios
       .get("https://allforyouapinodejs.herokuapp.com/api/product")
@@ -17,43 +17,56 @@ function ProductListContainer(props) {
 
   return (
     <React.Fragment>
-
-      {
-       products.length===0?(
+      {products.length === 0 ? (
         <div className={ProductListContainerStyle.container}>
-        <div className={ProductListContainerStyle.content}>
-
-          {loader.map((product) => {
-            return (
-              <div className={ProductListContainerStyle.card}>
-                  <div >
-                <ContentLoader
-                  speed={2}
-                  width="100%"
-                  height="100%"
-                  
-                  backgroundColor="#999494"
-                  foregroundColor="#ecebeb"
-  
-                >
-                <rect x="0" y="0" rx="10" ry="10" width="100%" max-height="calc(100vh - 80px)" />  
-                </ContentLoader>
+          <div className={ProductListContainerStyle.content}>
+            {loader.map((product) => {
+              return (
+                <div className={ProductListContainerStyle.card}>
+                  <div>
+                    <ContentLoader
+                      speed={2}
+                      width="100%"
+                      height="100%"
+                      backgroundColor="#999494"
+                      foregroundColor="#ecebeb"
+                    >
+                      <rect
+                        x="0"
+                        y="0"
+                        rx="10"
+                        ry="10"
+                        width="100%"
+                        max-height="calc(100vh - 80px)"
+                      />
+                    </ContentLoader>
                   </div>
-                  
-
 
                   <div className={ProductListContainerStyle.description}>
                     <div className={ProductListContainerStyle.name}>
                       <ContentLoader
-                            speed={2}
-                            width="90%"
-                            height="100%"
-                          
-                            backgroundColor="#999494"
-                            foregroundColor="#ecebeb"
+                        speed={2}
+                        width="90%"
+                        height="100%"
+                        backgroundColor="#999494"
+                        foregroundColor="#ecebeb"
                       >
-                        <rect x="0" y="10" rx="10" ry="10" width="100%" height="10" /> 
-                        <rect x="0" y="30" rx="10" ry="10" width="60%" height="10" /> 
+                        <rect
+                          x="0"
+                          y="10"
+                          rx="10"
+                          ry="10"
+                          width="100%"
+                          height="10"
+                        />
+                        <rect
+                          x="0"
+                          y="30"
+                          rx="10"
+                          ry="10"
+                          width="60%"
+                          height="10"
+                        />
                       </ContentLoader>
                     </div>
                     <div className={ProductListContainerStyle.price}>
@@ -63,25 +76,26 @@ function ProductListContainer(props) {
                         height="100%"
                         backgroundColor="#999494"
                         foregroundColor="#ecebeb"
-                        >
-
-                      <rect x="20%" y="30" rx="10" ry="10" width="60%" height="30%" />
-                    </ContentLoader> 
-                      
-                     </div>
+                      >
+                        <rect
+                          x="20%"
+                          y="30"
+                          rx="10"
+                          ry="10"
+                          width="60%"
+                          height="30%"
+                        />
+                      </ContentLoader>
+                    </div>
                   </div>
-              
-              </div>
-            );
-          })}
-
+                </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
-
-       ):(
+      ) : (
         <div className={ProductListContainerStyle.container}>
           <div className={ProductListContainerStyle.content}>
-
             {products.map((product) => {
               return (
                 <div className={ProductListContainerStyle.card}>
@@ -108,11 +122,9 @@ function ProductListContainer(props) {
                 </div>
               );
             })}
-
           </div>
         </div>
       )}
-
     </React.Fragment>
   );
 }
