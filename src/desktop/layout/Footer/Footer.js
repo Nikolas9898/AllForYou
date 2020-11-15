3d87b667b0a3b4acdd5f1e52014aa7be22ec970b
Nikolas9898 <nikolanikolchov@gmail.com>
2020-11-15 16:03:38 +0200

    added right number in footer

--- src/desktop/layout/Footer/Footer.js
+++ src/desktop/layout/Footer/Footer.js
@@ -3,39 +3,38 @@ import FooterStyl from "./FooterStyl.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import econt from '../../../images/econt.jpg';
+import econt from "../../../images/econt.jpg";
 
 function Footer(props) {
-    return (
-         <footer className={FooterStyl.container}>
-           <div className={FooterStyl.content}>
-           
-            <adress className={FooterStyl.connect_card}>
-                <h1>
-                    Свържете се с нас
-                </h1>
-                <div className={FooterStyl.connect_form}>
-                     <FontAwesomeIcon className={FooterStyl.connect} icon={faPhone} />
-                     <a className={FooterStyl.connect} href="tel:0876825221">0876825221</a>
-                </div>
-                 <div className={FooterStyl.connect_form}>
-                     <FontAwesomeIcon className={FooterStyl.connect} icon={faEnvelope} />
-                      <a className={FooterStyl.connect} href="mailto:iponwebschool@gmail.com">zaveskiponeshto@gmail.com</a>
-                    
-                 </div>
-          </adress>
-
-
-          <div className={FooterStyl.connect_card}>
-               <h1>
-                    Доставяме чрез
-               </h1>
-                    <img className={FooterStyl.logo} src={econt}/>
+  return (
+    <footer className={FooterStyl.container}>
+      <div className={FooterStyl.content}>
+        <adress className={FooterStyl.connect_card}>
+          <h1>Свържете се с нас</h1>
+          <div className={FooterStyl.connect_form}>
+            <FontAwesomeIcon className={FooterStyl.connect} icon={faPhone} />
+            <a className={FooterStyl.connect} href="tel:0876825221">
+              0883771333
+            </a>
           </div>
-        
-           </div>
-        </footer>
-    );
-  }
-  
-  export default Footer;
+          <div className={FooterStyl.connect_form}>
+            <FontAwesomeIcon className={FooterStyl.connect} icon={faEnvelope} />
+            <a
+              className={FooterStyl.connect}
+              href="mailto:iponwebschool@gmail.com"
+            >
+              zaveskiponeshto@gmail.com
+            </a>
+          </div>
+        </adress>
+
+        <div className={FooterStyl.connect_card}>
+          <h1>Доставяме чрез</h1>
+          <img className={FooterStyl.logo} src={econt} />
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
